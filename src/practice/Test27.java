915228d2d6f7e9486de3533d15b0dd7650a81105
SunnyNirwan14 <sunny2nirwan@gmail.com>
2022-12-06 13:52:41 +0530

    1st commit

--- src/practice/Test27.java
+++ src/practice/Test27.java
@@ -6,21 +6,7 @@ public class Test27
 	
 	public static void main(String[] args) 
 	{
-		String input = "kiran nirwan";
-        String output ="";
-
-        for (int i = 0; i < input.length(); i++) 
-        {
-            for (int j =0; j < output.length(); j++)
-            {
-                if (input.charAt(i) != output.charAt(j)) 
-                {
-                    output = output + input.charAt(i);
-                }
-            }
-        }
-
-        System.out.println(output);
+		
 	}
 
 }
